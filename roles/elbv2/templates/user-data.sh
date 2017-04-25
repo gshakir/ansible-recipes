8ae95da4803bbfbe74636fc6f4c090b4a7664092
Gulam Shakir <gshakir@me.com>
2017-04-24 22:57:39 -0400

    Save elb

---
+++ roles/elbv2/templates/user-data.sh
@@ -0,0 +1,20 @@
+
+# Standalone server
+#
+ruby -e 'require "webrick"
+
+class Echo < WEBrick::HTTPServlet::AbstractServlet
+  def initialize(server, name)
+    super server
+    @name = name
+  end
+  def do_GET(request, response)
+    response.status = 200
+    response.body = @name + ": " + request.path.to_s + "\n"
+  end
+end
+
+server = WEBrick::HTTPServer.new()
+server.mount "/", Echo, "{{ item.name }}"
+trap "INT" do server.shutdown end
+server.start' &
